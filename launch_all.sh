99581866b4f9668719aba44343884944884c5b18
duje13 <dujesol@gmail.com>
2021-01-07 14:10:30 +0100

    Improve to use relative paths

--- launch_all.sh
+++ launch_all.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #sleep 10
-conky -dq -c INSERT_PATH/time_rc
-conky -dq -c INSERT_PATH/system_rc
-conky -dq -c INSERT_PATH/processes_rc
-conky -dq -c INSERT_PATH/memory_rc
-conky -dq -c INSERT_PATH/filesystem_rc
-conky -dq -c INSERT_PATH/network_rc
-conky -dq -c INSERT_PATH/fortune_rc
-conky -dq -c INSERT_PATH/mediaplayer_rc
+cd $(dirname $BASH_SOURCE)
+
+conky -dq -c time_rc
+conky -dq -c system_rc
+conky -dq -c processes_rc
+conky -dq -c memory_rc
+conky -dq -c filesystem_rc
+conky -dq -c network_rc
+conky -dq -c fortune_rc
+#conky -dq -c mediaplayer_rc
