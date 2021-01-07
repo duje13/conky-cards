d5ba0d035b4bcc93776ca5feb68b1db7e9b659f9
duje13 <dujesol@gmail.com>
2021-01-07 14:00:17 +0100

    Modify and improve CPU

---
+++ lua_test.lua
@@ -0,0 +1,11 @@
+function conky_mycpus()
+    local file = io.popen("grep -c processor /proc/cpuinfo")
+    local numcpus = file:read("*n")
+    file:close()
+    listcpus = ""
+    for i = 1,numcpus
+     do listcpus = listcpus.."${color3}Core "..tostring(i)..": ${cpu cpu"..tostring(i).."}% ${alignr}${color1}${cpubar cpu"..tostring(i).." 6,140}\n"
+    end
+
+    return listcpus
+end
